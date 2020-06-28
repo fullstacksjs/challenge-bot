d8b52766e7f104e0f3479e7b4fb99c3961a9e0cc
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-06-28 12:09:28 +0430

    gitignore fixed

--- src/util/actions.js
+++ src/util/actions.js
@@ -1,9 +1,5 @@
 const actions = {
-<<<<<<< HEAD
   content: 'getting content',
-=======
-  description: 'getting description',
->>>>>>> 7b246ba98baaa72a664cd00426722c8d9867c91e
   title: 'getting title',
   options: 'getting options',
   correctAnswer: 'getting answer',
